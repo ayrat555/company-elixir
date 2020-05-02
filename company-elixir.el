d010ed9e83bff38e62eb4ff1d3725414213890e2
Ayrat Badykov <ayratin555@gmail.com>
2020-05-02 18:51:13 +0300

    add iex process management

--- company-elixir.el
+++ company-elixir.el
@@ -49,23 +49,33 @@
 
 (defvar company-elixir--last-completion nil "The last expression that was completed.")
 
-(defvar company-elixir--process nil "Iex process.")
-
 (defvar company-elixir--callback nil "Company callback to return candidates to.")
+
+(defvar company-elixir--processes nil "Iex processes for different projects used by company-elixir.")
+
+(defun company-elixir--create-process(project-path)
+  "Create a new iex process for PROJECT-PATH."
+  (let* ((default-directory project-path)
+         (process (start-process-shell-command project-path project-path company-elixir-iex-command))
+         (key-value-pair (cons project-path process)))
+    (set-process-query-on-exit-flag process nil)
+    (process-send-string process company-elixir--evaluator-init-code)
+    (set-process-filter process #'company-elixir--candidates-filter)
+    (setq company-elixir--processes (assoc-delete-all project-path company-elixir--processes))
+    (setq company-elixir--processes (cons key-value-pair company-elixir--processes))
+    process))
+
+(defun company-elixir--process (project-path)
+  "Return an iex process if it exists or create a new one otherwise for PROJECT-PATH."
+  (let ((existing-process (cdr (assoc project-path company-elixir--processes))))
+    (if (and existing-process (process-live-p existing-process))
+        existing-process
+      (company-elixir--create-process project-path))))
 
 (defun company-elixir--init-code()
   "Read iex evaluator if it's not read or return if it's read."
   (or company-elixir--evaluator-init-code
       (setq company-elixir--evaluator-init-code (company-elixir--read-init-code))))
-
-(defun company-elixir--start-iex-process ()
-  "Start iex process."
-  (let* ((process-name "iex")
-         (default-directory (company-elixir--project-root)))
-    (setq company-elixir--process (start-process-shell-command process-name "*iex*" company-elixir-iex-command))
-    (set-process-query-on-exit-flag company-elixir--process nil)
-    (process-send-string company-elixir--process company-elixir--evaluator-init-code)
-    (set-process-filter company-elixir--process #'company-elixir--candidates-filter)))
 
 (defun company-elixir--candidates-filter (_process output)
   "Filter OUTPUT from iex process and redirect them to company."
@@ -78,9 +88,7 @@
 
 (defun company-elixir--find-candidates(expr)
   "Send request for completion to iex process with EXPR."
-  (unless company-elixir--process
-    (company-elixir--start-iex-process))
-  (process-send-string company-elixir--process
+  (process-send-string (company-elixir--process (company-elixir--project-root))
                        (concat "CompanyElixirServer.expand('" expr "')\n")))
 
 (defun company-elixir (command &optional arg &rest ignored)
@@ -89,13 +97,12 @@
   (cl-case command
     (interactive (company-begin-backend 'company-elixir))
     (prefix (and (eq major-mode company-elixir-major-mode)
-                 (company-elixr--get-prefix)))
+                 (company-elixir--get-prefix)))
     (candidates (cons :async
                       (lambda (callback)
                         (setq company-elixir--callback callback)
                         (setq company-elixir--last-completion arg)
-                        (company-elixir--find-candidates arg))))
-    (annotation "")))
+                        (company-elixir--find-candidates arg))))))
 
 (defun company-elixir--return-candidates (candidates)
   "Return CANDIDATES to company-mode."
@@ -107,10 +114,14 @@
                                 (last-part (car (last parts)))
                                 (last-part-regex (concat last-part "$")))
                            (replace-regexp-in-string last-part-regex "" company-elixir--last-completion)))))
-             (completions (mapcar (lambda(var) (concat prefix var)) candidates)))
-        (funcall company-elixir--callback completions))))
+             (completions (mapcar
+                           (lambda(var)
+                             (concat prefix
+                                     (replace-regexp-in-string "/[[:digit:]]$" "" var))) candidates))
+             (completions-without-dups (cl-remove-duplicates completions :test #'equal)))
+        (funcall company-elixir--callback completions-without-dups))))
 
-(defun company-elixr--get-prefix ()
+(defun company-elixir--get-prefix ()
   "Return the expression under the cursor."
   (if (or (looking-at "\s") (eolp))
       (let (p1 p2 (skip-chars "-_A-Za-z0-9.?!@:"))
